992b057a07a8a151b8793263682f85302f36dde7
Amir Nickroo <amiracledev@gmail.com>
2018-10-11 14:50:35 -0700

    Initial Commit

---
+++ AmiraclePodcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmiraclePodcast
+//
+//  Created by Amir Nickroo on 10/11/18.
+//  Copyright © 2018 Amir Nickroo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

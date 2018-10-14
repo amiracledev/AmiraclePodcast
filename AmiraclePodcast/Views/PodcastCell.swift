32a036e2f55a339776c57bebd138549489a87156
Amir Nickroo <amiracledev@gmail.com>
2018-10-14 08:05:11 -0700

    updating cell

--- AmiraclePodcast/Views/PodcastCell.swift
+++ AmiraclePodcast/Views/PodcastCell.swift
@@ -19,6 +19,7 @@ class PodcastCell: UITableViewCell {
         didSet {
             trackNameLabel.text = podcast.trackName
             artistNameLabel.text = podcast.artistName
+            
         }
     }
     
